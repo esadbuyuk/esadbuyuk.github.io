25083f31e5f1e4c802318e9dbe43a462e7b629e9
esadbuyuk <mahmudesadbuyuk@gmail.com>
2025-05-18 11:04:44 +0300

    improving camera on web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1662138048"
+    serviceWorkerVersion: "3807806862"
   }
 });
