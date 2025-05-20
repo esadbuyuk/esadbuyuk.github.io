b674c7dfd9807ea4e1b722ff2446cac64d2fbe18
esadbuyuk <mahmudesadbuyuk@gmail.com>
2025-05-20 15:11:53 +0300

    again fixed throwing error issues for camera

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1491096410"
+    serviceWorkerVersion: "1295503360"
   }
 });
