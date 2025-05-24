eee401c22a200d073efb6b52f54296f1977f5dc1
esadbuyuk <mahmudesadbuyuk@gmail.com>
2025-05-24 11:54:49 +0300

    kalite max, çözünürlük: 1280 + cropping

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3253480972"
+    serviceWorkerVersion: "2486811163"
   }
 });
