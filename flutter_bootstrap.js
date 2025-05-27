8841469fc8410a577d4e5479f5ed894b2c02fc44
esadbuyuk <mahmudesadbuyuk@gmail.com>
2025-05-27 10:30:08 +0300

    checking camera

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1005859518"
+    serviceWorkerVersion: "1215332861"
   }
 });
