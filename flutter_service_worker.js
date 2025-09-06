b635b4f045883ae15d54c7a9664805c5fa709122
esadbuyuk <esatbuyuk@hotmail.com>
2025-09-06 16:44:20 +0300

    images problem fixed

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -45,7 +45,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "47891071fb8904cf46b8cfcfa48c4074
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "6be11c025cfe2a2e7bcbaf75b1c5b767",
+"flutter_bootstrap.js": "4a0797754b3143eb491e3be2809dd95c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
