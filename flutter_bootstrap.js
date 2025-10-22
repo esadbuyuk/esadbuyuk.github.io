85f74a3a1fc7e26e998bbd1fc7b115fa7e3d85b1
esadbuyuk <esatbuyuk@hotmail.com>
2025-10-22 11:38:02 +0300

    video problem fixed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2299520871"
+    serviceWorkerVersion: "3979685799"
   }
 });
