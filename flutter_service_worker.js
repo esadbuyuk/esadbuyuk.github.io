dc380f134c60ce058c92b90d21a55e48c8d4fd45
esadbuyuk <esatbuyuk@hotmail.com>
2025-11-09 10:42:09 +0300

    some adjustments for uı playbook videos

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -54,14 +54,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "007afa876a93c97f49b9df5587330b6b
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "530ebfa500515f7bbce4748fce2f081a",
+"flutter_bootstrap.js": "e3fe639bcbacf9279353148e1c212d04",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "b2c3424e0678a720c3fe00b3b27997bb",
 "/": "b2c3424e0678a720c3fe00b3b27997bb",
-"main.dart.js": "0b5846a813c7a19b87ab1bd402d1f917",
+"main.dart.js": "8fb3315094bf432e79bea9fca82fb4d7",
 "manifest.json": "ad767fee01bc3bc541d3dd66022a2530",
 "version.json": "a5c15c3298c547e01ee1b75a83da5b1d"};
 // The application shell files that are downloaded before a service worker can
