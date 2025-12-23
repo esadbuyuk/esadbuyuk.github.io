154a4e78f9dcd480b521c8873e96963f02e43d87
esadbuyuk <esatbuyuk@hotmail.com>
2025-12-23 13:45:06 +0300

    camera is working faster because of thread system

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1586570469"
+    serviceWorkerVersion: "3831882787"
   }
 });
