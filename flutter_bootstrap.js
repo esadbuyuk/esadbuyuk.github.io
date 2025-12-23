395040f8a3d5eb091ba6de5477a9d072a281c638
esadbuyuk <esatbuyuk@hotmail.com>
2025-12-23 14:30:35 +0300

    loading system fixed for camera Page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3831882787"
+    serviceWorkerVersion: "4237993031"
   }
 });
