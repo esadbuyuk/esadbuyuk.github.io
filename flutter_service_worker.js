c1da913547135161665ae5208fc6b575721f0635
esadbuyuk <esatbuyuk@hotmail.com>
2025-12-26 01:32:42 +0300

    solved some mobile api entegration problems

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -56,14 +56,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "5e478b6c98b5e6e5c444c7885975c1c6
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "d7be14f3e88861d950afc979d71d7b8c",
+"flutter_bootstrap.js": "9ca61c1a5f4e6061f71afca88403ce1e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "b2c3424e0678a720c3fe00b3b27997bb",
 "/": "b2c3424e0678a720c3fe00b3b27997bb",
-"main.dart.js": "9e0d0aaff80b4eb830fab0d07a372f88",
+"main.dart.js": "9087119b9203f804257df42f46e87d50",
 "manifest.json": "ad767fee01bc3bc541d3dd66022a2530",
 "version.json": "a5c15c3298c547e01ee1b75a83da5b1d"};
 // The application shell files that are downloaded before a service worker can
