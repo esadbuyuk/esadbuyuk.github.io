c16e2472944db1d283fccf0857dffd8d966b6fdb
esadbuyuk <esatbuyuk@hotmail.com>
2026-01-12 09:24:46 +0300

    localStorage added for opticResult

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -56,14 +56,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "5e478b6c98b5e6e5c444c7885975c1c6
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "d272ed118af76fbdba207a7683173cf3",
+"flutter_bootstrap.js": "4e276f574e3d7b026b2f23f10e5f9f72",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "b2c3424e0678a720c3fe00b3b27997bb",
 "/": "b2c3424e0678a720c3fe00b3b27997bb",
-"main.dart.js": "9a8f3cdcf29ef258b6e1301addd8247c",
+"main.dart.js": "32677433d6f4a22eaf6f9de4e5712019",
 "manifest.json": "ad767fee01bc3bc541d3dd66022a2530",
 "version.json": "a5c15c3298c547e01ee1b75a83da5b1d"};
 // The application shell files that are downloaded before a service worker can
