a34a3f2e9b0b1dd50fc3aa2102c9df01d3ad59db
esadbuyuk <esatbuyuk@hotmail.com>
2026-01-30 19:07:59 +0300

    video processing added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "76599772"
+    serviceWorkerVersion: "1649603672"
   }
 });
