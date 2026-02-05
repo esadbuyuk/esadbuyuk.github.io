02f6ce000bd83fdfd3c99d27f4e26e2f46d68f83
esadbuyuk <esatbuyuk@hotmail.com>
2026-02-05 18:54:06 +0300

    solution page fixed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1264601743"
+    serviceWorkerVersion: "2683475358"
   }
 });
