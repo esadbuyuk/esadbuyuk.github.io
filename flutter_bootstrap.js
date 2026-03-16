e36a9eefcccc59298b9ee55d11cb9f7901b988e7
esadbuyuk <esatbuyuk@hotmail.com>
2026-03-17 02:06:47 +0300

    fixed some score and sub skills problems

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3077246758"
+    serviceWorkerVersion: "3788317282"
   }
 });
